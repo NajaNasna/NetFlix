87345ea014419fbf95575be21e75865920cd32f7
NajaNasna <najanazir453@gmail.com>
2024-01-10 13:38:35 +0530

    modified

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -9,6 +9,7 @@ function Banner() {
 
   useEffect(() =>{
     axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response)
         console.log(response.data.results[0])
         setMovie(response.data.results[
           Math.floor(Math.random()*response.data.results.length)
